03ac479256ea0947afce906f1e04013e8aba066f
Haikeys <haikeysfpl@gmail.com>
2026-02-24 22:38:31 +0100

    security: add strict HTTP security headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict\u002DTransport\u002DSecurity',
+            value: 'max\u002Dage=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X\u002DFrame\u002DOptions',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X\u002DContent\u002DType\u002DOptions',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer\u002DPolicy',
+            value: 'origin\u002Dwhen\u002Dcross\u002Dorigin'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
